dc46df7d7ba139fc17110065f28228db39ac34cb
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-12-16 20:54:38 -0600

    test

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public List<string> Values { get; set; }
+
+        public Class1()
+        {
+            Values = new List<string>();
+            Username = string.Empty;
+            Email = "test";
+        }
+        public string GetEmail()
+        {
+            Console.WriteLine(Email);
+            return Email;
+        }
+        public string GetUsername()
+        {
+            Console.WriteLine(Username);
+            return Username;
+        }
+        public void SetUsername(string name)
+        {
+            Username = name;
+            
+        }
+        public void AddValue(string str)
+        {
+            Values.Add(str);
+        }
+        public void PrintVals()
+        {
+            foreach(var val in Values)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Windows.Markup;
+
+namespace ConsoleApp1
+{
+    class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Class1 user = new Class1();
+            user.AddValue("some val");
+            user.AddValue("another val");
+
+            user.PrintVals();
+            
+        }
+    }
+}
